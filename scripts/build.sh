6e61c5f8959229980e4b2e3243c2845a101fa938
Francisco Puga <fpuga@icarto.es>
2026-02-20 17:27:22 +0100

    Use umami as analytics system

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-docker run --rm -e CARDS=true -v "${PWD}":/docs squidfunk/mkdocs-material build
-# Add symlink to preserve old feed url operation
-# cd site && cp feed_rss_created.xml feed.xml
+
+ANALYTICS=true mkdocs build
